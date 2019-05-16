b7ab9ac58cdce5967b4904fa909e9db9c0fa58fd
ice <ice@163.com>
2019-05-16 22:18:13 +0800

    数据的增删改查

--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from .models import book,hero
 #MVT中的V，便于用户查看
@@ -25,3 +25,59 @@ def deatil(request,id):
     temp = loader.get_template('booktest/deatil.html')
     result = temp.render({"books":books})
     return HttpResponse(result)
+def deletebook(request,id):
+    # return HttpResponse("success")
+    book.objects.get(pk=id).delete()
+    return HttpResponseRedirect('/booktest/list/')
+def deletehero(request,id):
+    heros=hero.objects.get(pk=id)
+    bookid=heros.wj.id
+    heros.delete()
+    # return HttpResponse("success")
+    return HttpResponseRedirect('/booktest/deatil/%s'%bookid)
+def addhero(request,id):
+    if request.method=="GET":
+        return render(request,'booktest/addhero.html',{"bookid":id})
+    elif request.method=="POST":
+        chosebook=book.objects.get(pk =id)
+        newhero=hero()
+        newhero.name=request.POST["heroname"]
+        newhero.gender=request.POST['sex']
+        newhero.skill=request.POST['skill']
+        newhero.wj=chosebook
+        newhero.save()
+        return HttpResponseRedirect("/booktest/deatil/%s/"%id)
+def addbook(request):
+    if request.method=="GET":
+        return render(request,'booktest/addbook.html')
+    elif request.method=="POST":
+        newbook=book()
+        newbook.title=request.POST["bookname"]
+        newbook.pub_date=request.POST["pub_date"]
+        newbook.save()
+        return HttpResponseRedirect("/booktest/list/")
+def heroupdate(request,id):
+    if request.method=="GET":
+        return render(request,'booktest/heroupdate.html',{"heroid":id})
+    elif request.method=="POST":
+        print("post")
+        newhero=hero.objects.get(pk=id)
+        bookid=newhero.wj.id
+        newhero.name = request.POST["heroname"]
+        newhero.gender = request.POST['sex']
+        newhero.skill = request.POST['skill']
+        newhero.save()
+        return HttpResponseRedirect("/booktest/deatil/%s/"%bookid)
+def bookupdate(request,id):
+    if request.method=="GET":
+        return render(request,'booktest/bookupdate.html',{"bookid":id})
+    elif request.method=="POST":
+        newbook=book.objects.get(pk=id)
+        newbook.title=request.POST["bookname"]
+        newbook.pub_date=request.POST["pub_date"]
+        newbook.save()
+        return HttpResponseRedirect("/booktest/list/")
+
+
+
+
