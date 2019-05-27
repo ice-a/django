4ecebd2b39d4565af3d1350474583ef98764a414
ice <ice@163.com>
2019-05-27 10:51:14 +0800

    先备份一下

--- demo4/demo4/wsgi.py
+++ demo4/demo4/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for demo4 project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/0002.0002/howto/deployment/wsgi/
 """
 
 import os
